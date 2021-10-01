85f08d4a230d1c323b8142e24bb8a5668a009f33
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-01 15:49:19 +0530

    Add files via upload

---
+++ integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int num1;
+int num2;
+printf("enter the integer 1:\n");
+scanf("%d",&num1);
+printf("enter the integer 2:\n");
+scanf("%d",&num2);
+printf("the first integer is %d \n",num1);
+printf("the second integer is %d \n",num2);
+return 0;
+}
