6e49dcf33de95978451fa7ac8ba78c51a67c6f80
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:16:49 +0530

    Add files via upload

---
+++ operation.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+float x,y,s;
+printf ("enter the x = ");
+scanf ("%f",&x);
+printf (" enter the y = ");
+scanf ("%f",&y);
+s= x+y;
+printf (" \nx+y = %f",s);
+s= x-y;
+printf (" \nx-y = %f",s);
+s= x*y;
+printf (" \nx*y = %f",s);
+s= x/y;
+printf (" \nx/y = %f",s);
+return 0;
+}
