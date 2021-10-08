28533885a4594b8c571e6f951152117149638d5e
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:04:21 +0530

    Add files via upload

---
+++ areaoftrap.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+float c,a,b,h,area;
+printf ("enter the c = ");
+scanf ("%f",&c);
+printf ("enter the a = ");
+scanf ("%f",&a);
+printf ("enter the b = ");
+scanf ("%f",&b);
+printf ("enter the h = ");
+scanf ("%f",&h);
+area = c*(a+b)*h;
+printf ("area=%f",area);
+return 0;
+}
+
