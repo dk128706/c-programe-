299586b9d3456f661e8bf7537ef9270d989c0592
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:08:33 +0530

    Add files via upload

---
+++ volofsphere.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+float c,p,r,volume;
+printf ("\nenter the c value = ");
+scanf ("%f",&c);
+printf ("\nenter the p value = ");
+scanf ("%f",&p);
+printf ("\nenter the r value = ");
+scanf ("%f",&r);
+volume = c*p*r*r;
+printf (" volume = %f",volume);
+return 0;
