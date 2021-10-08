2f7724278840a39c690010acf983397cdfbc7cd8
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:47:02 +0530

    Add files via upload

---
+++ Q-27.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+float num1;
+float num2,multiply;
+printf("Enter the value of Base: \n");
+scanf("%f",&num1);
+printf("Enter the value of Height: \n");
+scanf("%f",&num2);
+multiply= 0.5*num1*num2;
+printf("Area of Triangle: %f \n",multiply);
+return 0;
+}
+
