42a98583aabfa66984e6cb8ca7ddd32f17ec9de2
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:47:24 +0530

    Add files via upload

---
+++ Q-28.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float a,multiply;
+printf("Enter the side:\n");
+scanf("%f",&a);
+multiply= 0.4330*a*a;
+printf("Area of an Equilateral Triangle is %f \n",multiply);
+return 0;
+}
+
