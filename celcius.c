479fdceca686ec67d0697ea91ff1e4dfb0f7079f
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:35:01 +0530

    Add files via upload

---
+++ celcius.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float c,f;
+printf ("enter temperatue in celcius = ");
+scanf ("%f",&c);
+f = c*1.8+32;
+printf (" converted value = %f faranheits\n ",f);
+return 0;
+}
+
