24c8898b66bab6f8f3c6436adfbc35b10cdae914
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:21:30 +0530

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,perimeter;
+printf ("\nenter the legth a = ");
+scanf ("%d",&a);
+printf ("\nenter the breath b = ");
+scanf ("%d",&b);
+perimeter = 2*(a+b);
+printf ("perimeter = %d",perimeter);
+return 0;
+}
