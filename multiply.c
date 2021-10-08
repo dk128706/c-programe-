599b51f194e995b1d88b4789fc726ec1218e64cb
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 14:45:48 +0530

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+float num1;
+float num2;
+printf ("enter num1 : \n");
+scanf ("%f",&num1);
+printf ("enter num2 : \n");
+scanf ("%f",&num2);
+printf ("the multiply of num1 and num2 is %f",num1*num2);
+return 0;
+}
+
