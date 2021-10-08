a739fe0e07557a5e793171c23083bd0b8151c0a0
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:45:45 +0530

    Add files via upload

---
+++ Q-24.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int x,y,s;
+printf("Enter the value of x: \n");
+scanf("%d",&x);
+printf("Enter the value of y: \n");
+scanf("%d",&y);
+s= pow(x,y);
+printf("power of %d is %d \n",x,s);
+return 0;
+
+}
