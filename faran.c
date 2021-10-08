229269ad354eec2d64569e233415a4e759e78b5d
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:35:45 +0530

    Add files via upload

---
+++ faran.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float c,f;
+printf ("enter temperatue in faran = ");
+scanf ("%f",&f);
+c = (f-32)*5/9;
+printf (" converted value = %f celsius\n ",c);
+return 0;
+}
