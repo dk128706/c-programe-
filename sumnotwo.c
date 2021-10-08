da2a3d9d86403fbd906fa45bf076f5985710c6d3
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:14:11 +0530

    Add files via upload

---
+++ sumnotwo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,sum;
+printf ("enter the num1 = ");
+scanf ("%d",&num1);
+printf ("enter the num2 = ");
+scanf ("%d",&num2);
+sum = num1+num2;
+printf (" sum of num1,num2 = %d",sum);
+return 0;
+}
