2446b9a9d08e0678ec09321abc953534660f86ca
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:29:36 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+float r,p,d,c,a;
+printf ("\nenter the radius r =  ");
+scanf ("%f",&r);
+p=3.14;
+c=2*p*r;
+d=2*r;
+a=p*r*r;
+printf (" diameter = %f\n",d);
+printf (" circumference  = %f\n",c);
+printf (" area = %f\n",a);
+return 0;
+}
