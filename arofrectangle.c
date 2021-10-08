a25729de3c666b3e0ac0968d839646757aa11f2c
dk128706 <91726997+dk128706@users.noreply.github.com>
2021-10-08 15:25:56 +0530

    Add files via upload

---
+++ arofrectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,area;
+printf ("\nenter the legth a = ");
+scanf ("%d",&a);
+printf ("\nenter the breath b = ");
+scanf ("%d",&b);
+area = (a*b);
+printf ("area = %d",area);
+return 0;
+}
